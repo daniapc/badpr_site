2a5b67935e9c743be5eb52ad6e4d0d45e954c9b9
daniapc <daniel.castro0811@gmail.com>
2022-07-06 10:03:12 -0300

    Navbar routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,35 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
+  get 'home/noticias'
+
   get 'home/regras_simples'
+
+  get 'home/regras_completas'
+
+  get 'home/parabadminton'
+
+  get 'home/links'
+
+  get 'home/inscricao'
+
+  get 'home/certificados'
+
+  get 'home/ranking'
+
+  get 'home/calendario'
+
+  get 'home/onde_jogar'
+
+  get 'home/historia'
+
+  get 'home/estrutura'
+
+  get 'home/estatuto'
+
+  get 'home/regulamento'
+
+  get 'home/contato'
 
   root 'home#index'
 
